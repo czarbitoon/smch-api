512b70f81580f8d51c8b1eab7e82cc68d42a883a
czarbitoon <czarbitoon@gmail.com>
2025-01-19 17:03:11 +0800

    device controller

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -6,14 +6,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Device;
+use App\Models\Office;
 
 class DeviceController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'office_id' => 'required|exists:offices,id',
+        ]);
+
         $device = new Device();
         $device->name = $request->input('name');
         $device->description = $request->input('description');
+        $device->office_id = $request->input('office_id');
         $device->save();
         return response()->json(['message' => 'Device added successfully']);
     }
@@ -26,6 +34,11 @@ class DeviceController extends Controller
 
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
         $device = Device::find($id);
         $device->name = $request->input('name');
         $device->description = $request->input('description');
