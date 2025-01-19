f3a5611a303c33952503fe022060771207a1d5c9
czarbitoon <czarbitoon@gmail.com>
2025-01-19 17:05:02 +0800

    AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,5 +1,7 @@
 <?php
 
+// AuthController.php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -43,10 +45,6 @@ class AuthController extends Controller
         }
     }
 
-
-
-
-
     public function logout()
     {
         Auth::guard('api')->logout();
@@ -55,32 +53,31 @@ class AuthController extends Controller
     }
 
     public function register(Request $request)
-{
-    try {
-        $this->validate($request, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'string'],
-            'office_id' => 'required|exists:offices,id',
-        ]);
+    {
+        try {
+            $this->validate($request, [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+                'role' => ['required', 'string'],
+                'office_id' => 'required|exists:offices,id',
+            ]);
 
-        $validatedData = $request->only(['name', 'email', 'password', 'role']);
-        $validatedData['password'] = bcrypt($validatedData['password']);
+            $validatedData = $request->only(['name', 'email', 'password', 'role', 'office_id']);
+            $validatedData['password'] = bcrypt($validatedData['password']);
 
-        $user = User::create($validatedData);
+            $user = User::create($validatedData);
 
-        auth()->login($user);
+            auth()->login($user);
 
-        return response()->json([
-            'message' => 'Registration successful'
-        ], 200);
+            return response()->json([
+                'message' => 'Registration successful'
+            ], 200);
 
-    } catch (ValidationException $e) {
-        return response()->json(['error' => $e->getMessage()], 400);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
     }
-}
-
 
     protected function respondWithToken($token)
     {
@@ -92,10 +89,9 @@ class AuthController extends Controller
     }
 
     public function profile()
-{
-    $user = Auth::user(); // Get the authenticated user
+    {
+        $user = Auth::user(); // Get the authenticated user
 
-    return response()->json(['user' => $user], 200);
-}
-
+        return response()->json(['user' => $user], 200);
+    }
 }
