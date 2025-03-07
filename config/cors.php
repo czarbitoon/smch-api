505300cda2c4b43a05eda662a9e09cf900fc5613
czarbitoon <czarbitoon@gmail.com>
2025-03-07 22:10:07 +0800

    Device Fix

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,9 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:58149', 'http://localhost:*', 'http://localhost:8000'], // Frontend, Mobile and dynamic ports
-    'allowed_headers' => ['*', 'Authorization', 'X-Requested-With', 'Content-Type', 'Accept', 'Origin'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:63261', 'http://localhost:*', 'http://localhost:8000'], // Frontend, Mobile and dynamic ports
+    'allowed_headers' => ['*', 'Authorization', 'X-Requested-With', 'Content-Type', 'Accept', 'Origin', 'X-XSRF-TOKEN', 'X-CSRF-TOKEN'],
     'exposed_headers' => ['*'],
-    'max_age' => 0,
+    'max_age' => 7200,  // Increase cache time to improve performance
     'supports_credentials' => true,
 ];
