d8d799b2c3e5e4efe75f755462e11c2705ae09f8
czarbitoon <czarbitoon@gmail.com>
2026-05-07 20:03:43 +0800

    feat: update PostgreSQL connection settings for Supabase integration

--- config/database.php
+++ config/database.php
@@ -85,16 +85,19 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => env('DB_CHARSET', 'utf8'),
+            'host' => 'aws-1-ap-northeast-1.pooler.supabase.com', // Updated Host
+            'port' => '5432', // Updated Port for the IPv4 Proxy
+            'database' => 'postgres',
+            'username' => 'postgres.aetmsaviiieysmekjfct', // Updated Username (important!)
+            'password' => 'SMCHardwareMon',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
+            'options' => [
+                PDO::ATTR_EMULATE_PREPARES => true,
+            ],
         ],
 
         'sqlsrv' => [
